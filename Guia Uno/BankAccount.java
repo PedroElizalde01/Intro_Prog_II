1cee69bd99fec13ce3cc6e4bd38211bc3ca6ff84
PedroElizalde01 <pedro.elizalde@ing.austral.edu.ar>
2020-08-11 18:14:05 -0300

    Modif Guía 1 - BankAccount

--- Guia Uno/BankAccount.java
+++ Guia Uno/BankAccount.java
@@ -1,5 +1,31 @@
 public class BankAccount{
-  public static void main(String[] args) {
+  private String cbu;
+  private float balance;
 
+  public BankAccount(String cbu, float balance){
+    this.cbu = cbu;
+    this.balance = balance;
+  }
+
+  public static float deposit(float amount){
+    if (amount > 0){
+      balance = balance + amount;
+    } else{
+      // NO HACE NADA ESPECIAL -Pedro
+    }
+    return balance;
+  }
+  public static float withdraw(float amount){
+    if(amount < 0 && amount < balance){
+      balance = balance + amount;
+    } else {
+      System.out.println("Invalid amount, your balance is: " + balance);
+    }
+  }
+  public static void getBalance(){
+    return balance;
+  }
+  public static void getCBU(){
+    return cbu;
   }
 }
