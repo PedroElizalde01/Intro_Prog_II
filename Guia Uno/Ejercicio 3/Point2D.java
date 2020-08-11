c20426c3f615422e5704f4a82acc4a3d4e4a22c5
Pedro Elizalde Acevedo <68881110+PedroElizalde01@users.noreply.github.com>
2020-08-11 20:10:44 -0300

    Update Point2D.java

--- Guia Uno/Ejercicio 3/Point2D.java
+++ Guia Uno/Ejercicio 3/Point2D.java
@@ -7,13 +7,24 @@ public class Point2D {
     this.y = y;
   }
   public float getDistance(Point2D point) { //NO ENTIENDO BIEN ESTE- Pedro
+    x = this.x;
+    y = this.y;
 
-
+  }
+  public float add(Point2D point){
+    
   }
   public float getX(){
     return x;
   }
   public float getY(){
     return y;
+  }
+  public float getDistanceToOrigin(Point2D point){
+    x = point.getX();
+    y = point.getY();
+    n = (Math.pow((x-0)) + Math.pow((y-0)));
+    d = Math.sqr(n);
+    return d;
   }
 }
