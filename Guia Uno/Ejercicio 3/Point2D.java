60fb620c1f4b299a1c4d97a351350b55957df4ad
PedroElizalde01 <pedro.elizalde@ing.austral.edu.ar>
2020-08-11 19:35:00 -0300

    Modif Guía 1 - Ejercicio3

---
+++ Guia Uno/Ejercicio 3/Point2D.java
@@ -0,0 +1,19 @@
+public class Point2D {
+  private float x;
+  private float y;
+
+  public Point2D (float x, float y) {
+    this.x = x;
+    this.y = y;
+  }
+  public float getDistance(Point2D point) {
+
+
+  }
+  public float getX(){
+    return x;
+  }
+  public float getY(){
+    return y;
+  }
+}
