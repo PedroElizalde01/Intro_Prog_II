99e59d0781779d6d586bcdae3874b08af22a5283
PedroElizalde01 <pedro.elizalde@ing.austral.edu.ar>
2020-08-11 22:50:52 -0300

    Intro_Prog_II

---
+++ Guia Uno/Ejercicio 4/Circle.java
@@ -0,0 +1,9 @@
+public class Circle {
+  private float radius;
+  private float centre;
+
+  public Circle (float radius, float center) {
+    this.radius = radius;
+    this.center = center;
+  }
+}
