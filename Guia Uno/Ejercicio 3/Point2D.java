0a83486c5ddf42e004b7fa806a76fecb67be977a
PedroElizalde01 <pedro.elizalde@ing.austral.edu.ar>
2020-08-23 21:32:59 -0300

    Modif Ejercicio3

--- Guia Uno/Ejercicio 3/Point2D.java
+++ Guia Uno/Ejercicio 3/Point2D.java
@@ -6,20 +6,35 @@ public class Point2D {
     this.x = x;
     this.y = y;
   }
-  public String getDistance(Point2D point) { // NO ENTIENDO BIEN ESTE- Pedro
-    double x = point.getX();
-    double y = point.getY();
-    return ("("+x+" ; "+y+")");
 
+  public double getDistance(Point2D point2){
+    double xcord = Math.pow((x - point2.getX()), 2);
+    double ycord = Math.pow((y - point2.getY()), 2);
+    double total = Math.sqrt(xcord + ycord);
+    return total;
   }
-  //public double add(Point2D point){
 
-  //}
+  public Point2D add(Point2D point){
+    double xcord = x + point.getX();
+    double ycord = y + point.getY();
+    Point2D point2 = new Point2D(xcord, ycord);
+    return point2;
+  }
+
   public double getX(){
     return x;
   }
+
   public double getY(){
     return y;
+  }
+
+  public void setX(double x){
+    this.x = x;
+  }
+
+  public void setY(double y){
+    this.y = y;
   }
   public double getDistanceToOrigin(Point2D point){
     double x = point.getX();
@@ -27,4 +42,5 @@ public class Point2D {
     double d = Math.sqrt((Math.pow(x, 2)) + (Math.pow(y, 2)));
     return d;
   }
+//  public void maxDistanceToOrigin(){}  PENSAR ESTE - PedroS
 }
